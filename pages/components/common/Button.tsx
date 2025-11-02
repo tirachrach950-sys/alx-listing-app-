b50b119902a91b580d31157308210487aaf8bef9
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-02 20:16:34 +0100

    Update Button.tsx

--- pages/components/common/Button.tsx
+++ pages/components/common/Button.tsx
@@ -1,1 +1,24 @@
+// components/common/Button.tsx
+import React from "react";
+
+interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick, type = "button", className }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition ${className}`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
 
