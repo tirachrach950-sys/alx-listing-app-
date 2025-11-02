6e30e9422a6b2c49cd1cb0c8e2a3e69c242d5066
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-02 20:09:18 +0100

    Update index.ts

--- pages/components/common/interfaces/index.ts
+++ pages/components/common/interfaces/index.ts
@@ -1,1 +1,14 @@
+// interfaces/index.ts
+
+export interface CardProps {
+  title: string;
+  description: string;
+  imageUrl?: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+}
 
